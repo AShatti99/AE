eb2dbdb67d8d9b217e3bd2a4b7c1df348a678d9b
AShatti99 <52958874+AShatti99@users.noreply.github.com>
2021-01-19 16:39:33 +0100

    Create print.s

---
+++ ARMv7/Liste/stampa/print.s
@@ -0,0 +1,22 @@
+        .data
+fmt1:   .string "%d -> "
+fmt2:   .string "Fine lista\n"
+        .text
+        .global print
+
+print:  push {lr}
+
+loop:   cmp r0, #0          @ se la lista e' vuota
+        beq end             @ vai a end
+        ldr r1, [r0], #4    @ r0 -> next
+        push {r0, r1}           
+        ldr r0, =fmt1       
+        bl printf           @ stampa fmt1
+        pop {r0, r1}
+        ldr r0, [r0]        @ scorri la lista
+        b loop
+
+end:    ldr r0, =fmt2       @ stampa fmt2
+        bl printf
+        pop {lr}
+        bx lr   
