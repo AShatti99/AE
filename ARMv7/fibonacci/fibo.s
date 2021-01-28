fcd444fe2e6f841f7bbf6f77ecaf139ab5c49e9b
AShatti99 <52958874+AShatti99@users.noreply.github.com>
2021-01-28 17:26:06 +0100

    Create fibo.s

---
+++ ARMv7/fibonacci/fibo.s
@@ -0,0 +1,28 @@
+        .data
+fmt:    .string "Fibo(4) = %d\n"
+        .text
+        .global main
+
+main:   push {lr}
+        mov r0, #4
+        bl fibo
+
+end:    mov r1, r0
+        ldr r0, =fmt
+        bl printf
+        pop {lr}
+        bx lr
+
+
+fibo:   cmp r0, #1
+        movle pc, lr
+        sub r0, r0, #1
+        push {r0, lr}
+        bl fibo
+        pop {r1}
+        push {r0}
+        sub r0, r1, #1
+        bl fibo
+        pop {r1}
+        add r0, r0, r1
+        pop {pc}
