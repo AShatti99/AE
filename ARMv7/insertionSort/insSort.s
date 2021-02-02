e7061c1be95b150638adae2de09ca67ff161f724
AShatti99 <52958874+AShatti99@users.noreply.github.com>
2021-02-02 18:19:52 +0100

    Update insSort.s

--- ARMv7/insertionSort/insSort.s
+++ ARMv7/insertionSort/insSort.s
@@ -14,7 +14,7 @@ loop1:  cmp r1, r0              @ j < lunghezza stringa
         beq end
         ldrb r2, [r4, r1]       @ key = a[j];
         mov r3, r1
-        sub r3, r3, #1          @ i = j - 1
+        sub r3, r3, #1          @ i = j -1
         push {r0, r1}
 
 loop2:  cmp r3, #0              @ se i < 0
@@ -24,8 +24,7 @@ loop2:  cmp r3, #0              @ se i < 0
         ble fine                @ vai a fine
         mov r1, r3
         add r1, r1, #1          @ i+1
-        mov r5, r0
-        strb r5, [r4, r1]       @ a[i+1]=a[i]
+        strb r0, [r4, r1]       @ a[i+1]=a[i]
         sub r3, r3, #1          @ i--
         b loop2
 
@@ -33,8 +32,7 @@ fine:   cmp r3, #-1
         moveq r1, #0
         movne r1, r3
         addne r1, r1, #1        @ i+1
-        mov r5, r2
-        strb r5, [r4, r1]       @ a[i+1]=key
+        strb r2, [r4, r1]       @ a[i+1]=key
         pop {r0, r1}
         add r1, r1, #1          @ j++
         b loop1
