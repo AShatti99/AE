caadcad03de2e73533223e347e492a333e305ef8
AShatti99 <52958874+AShatti99@users.noreply.github.com>
2021-06-19 15:33:57 +0200

    Create mediaVoti.s

---
+++ ARMv7/mediaVoti.s
@@ -0,0 +1,26 @@
+@ Calcolo della media dei voti. I voti sono definiti in una direttiva .word e utilizziamo un valore speciale -1 per terminare la lista. 
+@ Si realizzi la divisione come funzione
+
+        .data
+v:      .word 30, 28, 27, 24, 28, 18, -1
+str:    .string "Media: %d\n"
+        .text
+        .global main
+
+main:   mov r0, #0              @ somma voti
+        ldr r1, =v
+        mov r3, #0              @ indice sui numeri da fare la media
+
+somma:  ldr r2, [r1], #4        @ carico un numero
+        cmp r2, #-1             @ controllo:
+        addne r0, r0, r2        @               se e' uguale a -1, lo inserisco nella nella somma
+        addne r3, r3, #1        @               e incremento l'indice
+        bne somma               @               e torno a somma
+
+        pusheq {lr}             @               altrimenti faccio la divisione
+        bleq div
+
+end:    mov r1, r0
+        ldr r0, =str
+        bl printf
+        pop {pc}
